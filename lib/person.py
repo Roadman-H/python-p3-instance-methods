f47828f88433eacdaf8b8cf8550d678dc7a1b08e
Hillary-Ndubi <hillarynyabaige@gmail.com>
2024-06-18 20:04:02 +0300

    Complete

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+    #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
 
-    #Instance method definition
-    pass
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
